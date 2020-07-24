a728d1b238c70e91bf6030e5444cc48da48f8a1c
poojithaT <poojitha121@gmail.com>
2020-07-24 04:45:58 -0400

    INNITIAL COMMIT

---
+++ server.js
@@ -0,0 +1,13 @@
+const  FAVOURITE_INSTRUCTOR = process.env.NAME;
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  while(true) {
+    console.log(`${FAVOURITE_INSTRUCTOR} rocks!`);
+    await sleep(5000);
+  }
+}
+
+main();
